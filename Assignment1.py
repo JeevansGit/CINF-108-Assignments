a1c140ad30af3e7906990275fde2aa410b3770f9
Jeevan <jeevan@Jeevans-MacBook-Pro.local>
2025-08-29 12:49:57 -0400

    commit

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+
+
+print("COURSE GRADING & ASSIGNMENT RULES")
+
+
+print("""
+    GRADING
+- Weekly assignments: ~80 precent of final grade
+- Midterm + Final Project: 20%
+  A 100–94 | A- 93–89 | B+ 88–85 | B 84–82 | B- 81–79
+  C+ 78–76 | C 75–73 | C- 72–70 | D+ 69–67 | D 66–64
+  D- 63–60 | E 59–0
+""")
+
+print("""
+    ASSIGNMENT RULES
+- Submit on time
+- Code must run; incomplete work loses points.
+- Repo must be public & source downloadable.
+- Screenshots may be required (.jpeg).
+- Work is individual.
+- Missed work = 0 (exceptions: medical note or religious observance).
+""")
